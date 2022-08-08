f791484ff8d3f676e559604170f221003b7641bd
Omar Rodriguez <omar.rguez26@gmail.com>
2022-08-08 13:03:13 -0400

    created User and set up Sequelize

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+// create our user model
+
+class User extends Model {}
+
+User.init(
+    {
+        //table column definitions got here
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        }, 
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be duplicate email in the db
+            unique: true,
+            // if allowNull is false, you can add validation
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    }, {
+        // table configuration goes here 
+        // pass in imported sequelize connection
+        sequelize,
+        // don't automatically create createdAt updatedAt fields
+        timestamps: false,
+        // dont plurize name of tables
+        freezeTableName: true,
+        // use undersscore instead of camelCase
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
