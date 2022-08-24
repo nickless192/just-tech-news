9c382db142ab15e8148b8999662632471173d540
Omar Rodriguez <omar.rguez26@gmail.com>
2022-08-24 18:54:49 -0400

    add helpers and testing

---
+++ __test__/helpers.test.js
@@ -0,0 +1,24 @@
+const {format_date, format_plural, format_url} = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2022-08-24 09:15:00');
+
+    expect(format_date(date)).toBe('8/24/2022');
+});
+
+test('format_plural() returns the plural when applicable', () => {
+    const word = "Tiger";
+    const amount = 2;
+
+    expect(format_plural(word, amount)).toBe("Tigers");
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+});
